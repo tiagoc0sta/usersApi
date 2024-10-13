22f5ec7426843dad93aaf4aa69dc93211f73b4b5
tiagoc0sta <tiagodpv@gmail.com>
2024-10-12 23:02:18 -0400

    create user model

--- src/main/java/com/tiagotfc/usersApi/Application.java
+++ src/main/java/com/tiagotfc/usersApi/Application.java
@@ -1,7 +1,10 @@
 package com.tiagotfc.usersApi;
 
+import com.tiagotfc.usersApi.users.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,6 +13,15 @@ public class Application {
 
 		SpringApplication.run(Application.class, args);
 
+	}
+
+	@Bean //instance of a class with metadata
+		// CommandLineRunner runs after application starter
+	CommandLineRunner user() {
+		return args -> {
+			User user = new User("tiagoc0sta", "Tiago", "Costa", "tiago@tiagotfc.com","654985777");
+			System.out.println(user);
+		};
 	}
 
 }
--- src/main/java/com/tiagotfc/usersApi/users/User.java
+++ src/main/java/com/tiagotfc/usersApi/users/User.java
@@ -1,4 +1,9 @@
 package com.tiagotfc.usersApi.users;
 
-public class User {
-}
+public record User(
+        String username,
+        String firstName,
+        String lastName,
+        String email,
+        String phone
+) {}
