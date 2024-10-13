2e51a76389c05a281f224272f69d0f97e1c12b06
tiagoc0sta <tiagodpv@gmail.com>
2024-10-13 15:15:49 -0400

    add docker-compose

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Run (
+   id INT NOT NULL,
+   user_name varchar(250) NOT NULL,
+   first_name varchar(250) NOT NULL,
+   last_name varchar(250) NOT NULL,
+   email varchar(250) NOT NULL,
+   phone varchar(250) NOT NULL,
+   PRIMARY KEY (id)
+);
